4bfd1e66157a0c7f14c86b4cdf45e89a71eadd07
Daniel Schulz <faulolio@gmail.com>
2022-06-25 22:19:31 +0200

    Format code

--- tests/SitemapTest.php
+++ tests/SitemapTest.php
@@ -6,9 +6,9 @@ use Slothsoft\Farah\Configuration\AssetConfigurationField;
 use Slothsoft\Farah\ModuleTests\AbstractSitemapTest;
 use Slothsoft\Farah\Module\Asset\AssetInterface;
 
-class SitemapTest extends AbstractSitemapTest
-{
-    protected static function loadSitesAsset() : AssetInterface {
+class SitemapTest extends AbstractSitemapTest {
+
+    protected static function loadSitesAsset(): AssetInterface {
         return (new AssetConfigurationField('farah://slothsoft@schedule.slothsoft.net/sitemap'))->getValue();
     }
 }
