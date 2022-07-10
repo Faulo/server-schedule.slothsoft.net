41253605560d9a303542c6dbbe69e55dff158977
Dende <faulolio@gmail.com>
2022-07-10 19:19:34 +0200

    Set html cache duration

--- scripts/bootstrap.php
+++ scripts/bootstrap.php
@@ -2,6 +2,8 @@
 declare(strict_types = 1);
 
 use Slothsoft\Farah\Module\Module;
+use Slothsoft\Farah\HTTPResponse;
 
 Module::registerWithXmlManifestAndDefaultAssets('slothsoft@schedule.slothsoft.net', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'assets');
 
+HTTPResponse::cacheDurations()['application/xhtml+xml'] = 1;
