a895a4657b2d5d864796fe743bf4b016a53468ce
Daniel Schulz <faulolio@gmail.com>
2023-08-08 12:53:33 +0200

    Fix qr

--- src/Assets/QRBuilder.php
+++ src/Assets/QRBuilder.php
@@ -2,25 +2,28 @@
 declare(strict_types = 1);
 namespace Slothsoft\Server\Schedule\Assets;
 
-use Slothsoft\Core\IO\Writable\Adapter\FileWriterFromStringWriter;
-use Slothsoft\Core\IO\Writable\Delegates\StringWriterFromStringDelegate;
+use Slothsoft\Core\IO\Writable\Delegates\FileWriterFromFileDelegate;
 use Slothsoft\Farah\FarahUrl\FarahUrlArguments;
 use Slothsoft\Farah\Module\Asset\AssetInterface;
 use Slothsoft\Farah\Module\Asset\ExecutableBuilderStrategy\ExecutableBuilderStrategyInterface;
 use Slothsoft\Farah\Module\Executable\ExecutableStrategies;
 use Slothsoft\Farah\Module\Executable\ResultBuilderStrategy\FileWriterResultBuilder;
 use Slothsoft\Server\Schedule\ServerConfig;
+use chillerlan\QRCode\QRCode;
+use SplFileInfo;
 
 class QRBuilder implements ExecutableBuilderStrategyInterface {
 
     public function buildExecutableStrategies(AssetInterface $context, FarahUrlArguments $args): ExecutableStrategies {
         $text = $args->get('text', '');
 
-        $writer = function () use ($text): string {
-            return ServerConfig::printQR($text);
+        $writer = function () use ($text): SplFileInfo {
+            $file = tmp_file(__CLASS__);
+            (new QRCode(ServerConfig::getQROptions()))->render($text, $file);
+            return new SplFileInfo($file);
         };
 
-        $resultBuilder = new FileWriterResultBuilder(new FileWriterFromStringWriter(new StringWriterFromStringDelegate($writer)), 'qr.png');
+        $resultBuilder = new FileWriterResultBuilder(new FileWriterFromFileDelegate($writer), 'qr.png');
 
         return new ExecutableStrategies($resultBuilder);
     }
